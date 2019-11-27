83a0abda974ceb65bc66711875f59159c69f056c
Lukas Renckens <44546625+LukasRenckens@users.noreply.github.com>
2019-11-27 15:45:10 +0100

    Add files via upload

---
+++ 2019_11_20_125450_create_student_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentTable extends Migration
+{
+    /** hoi
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->double('studentennummer');
+            $table->text('klas');
+            $table->timestamps();
+        }); 
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('student');
+    }
+}
