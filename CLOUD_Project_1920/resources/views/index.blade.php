7020db9b072f32dca5ff2cbc1b90392526e81900
lukas <lukas@10.5.18.180>
2019-11-28 09:18:03 +0100

    index.blade.php view teogevoegd en opgeroepen in de controller

---
+++ CLOUD_Project_1920/resources/views/index.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Testview</title>
+    </head>
+    <body>
+        <h1>Test</h1>
+        <hr/>
+        <p>Dit is een test view.</br>
+           Hier komt naderhand het inlogscherm of menu naar de veschillende services. 
+        </p>
+    </body>
+</html>
