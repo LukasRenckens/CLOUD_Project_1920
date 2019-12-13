684b53caae2c27525779bc5cbc8627cb4bedbd71
lukas <lukas@10.5.18.180>
2019-12-13 17:38:14 +0100

    Algemene structuur python REST-service

--- REST_service/venv/Include/app.py
+++ REST_service/venv/Include/app.py
@@ -1,28 +1,57 @@
-from flask import Flask , request
+from flask import Flask, request, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Resource, Api
+import MySQLdb
 
 app = Flask(__name__)
 api = Api(app)
 
-class Gravitatie(Resource):
-        def get(self, planeet): #Moet get noemen
-                if planeet == "aarde":
-                        return 9.81
-                elif planeet == "maan":
-                        return 6
-                else:
-                        return 0
-
-api.add_resource(Gravitatie, "/gravitatie/<string:planeet>")
-
 @app.route('/')
-def hello_world():
-        return "Hello World"
+def index():
+    return render_template('index.html')
 
-@app.route('/hallo')
-def hello_world2():
-        return "Hello World2"
+@app.route('/klas1')
+def klas1():
+    db = MySQLdb.connect(host="localhost",
+                         user="root",
+                         passwd="",
+                         db="schoolproject")
+    cur = db.cursor()
+    cur.execute("SELECT * FROM klas WHERE id='1'")
+
+    data = cur.fetchall()
+    db.close()
+
+    return render_template('klas1.html', data=data)
+
+@app.route('/klas2')
+def klas2():
+    db = MySQLdb.connect(host="localhost",
+                         user="root",
+                         passwd="",
+                         db="schoolproject")
+    cur = db.cursor()
+    cur.execute("SELECT * FROM klas WHERE id='2'")
+
+    data = cur.fetchall()
+    db.close()
+
+    return render_template('klas1.html', data=data)
+
+@app.route('/klas3')
+def klas3():
+    db = MySQLdb.connect(host="localhost",
+                         user="root",
+                         passwd="",
+                         db="schoolproject")
+    cur = db.cursor()
+    cur.execute("SELECT * FROM klas WHERE id='3'")
+
+    data = cur.fetchall()
+    db.close()
+
+    return render_template('klas1.html', data=data)
 
 if __name__ == '__main__':
-        app.run()
+    app.run(debug=True)
 
